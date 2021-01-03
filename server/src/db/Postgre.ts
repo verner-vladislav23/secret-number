e26c14574ef8deb74200510e478066bf939676cf
Verner <verner.vladislav23@gmail.com>
2021-01-03 13:42:56 +0300

    connect PostgreSQL

---
+++ server/src/db/Postgre.ts
@@ -0,0 +1,25 @@
+import { Pool, PoolConfig } from 'pg';
+
+const defaultConfig: PoolConfig = {
+  user: 'a1',
+  host: 'localhost',
+  database: 'secret_number',
+  password: '',
+  port: 5432,
+};
+
+class DB {
+  private _databaseConfig: PoolConfig;
+  public pool: Pool;
+
+  constructor (config?: PoolConfig) {
+    this._databaseConfig = config || defaultConfig;
+    this.connect();
+  }
+
+  public connect (): void {
+    this.pool = new Pool(this._databaseConfig);
+  }
+}
+
+export default new DB();
