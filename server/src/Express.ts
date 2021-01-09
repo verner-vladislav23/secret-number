0f37e5a538bad78591502651097e46fa78cfce0f
Verner <verner.vladislav23@gmail.com>
2021-01-09 16:24:29 +0300

    SERVER: Added User controller

--- server/src/Express.ts
+++ server/src/Express.ts
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 import gameController from './controllers/GameController';
 import authController from './controllers/AuthController';
+import userController from './controllers/UserController';
 
 class Express {
   public app: express.Application;
@@ -25,9 +26,11 @@ class Express {
   private applyRoutes(): void {
     const authRouter = authController.getRouter();
     const gameRouter = gameController.getRouter();
+    const userRouter = userController.getRouter();
 
     this.app.use('/api/v1/auth', authRouter);
     this.app.use('/api/v1/game', gameRouter);
+    this.app.use('/api/v1/user', userRouter);
   }
 }
 
---
+++ server/src/controllers/UserController.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+
+import BaseController from "./BaseController";
+import authenticate from '../middleware/authenticate'
+import { User } from '../db/models';
+
+interface ModifiedRequest extends Request {
+  user?: any;
+}
+
+class UserController extends BaseController {
+  constructor() {
+    super();
+
+    this.router.get('/current', authenticate, this._current);
+  }
+
+
+  private _current (req: ModifiedRequest, res: Response, next: NextFunction): Response {
+    const user: User = req.user;
+
+    return res.status(200).json(user);
+  }
+}
+
+export default new UserController();
